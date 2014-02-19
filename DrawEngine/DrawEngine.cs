9667f3c06a60b6a9a0655b36ac0c2668314dbc09
lnxslackware <lnxslackware@gmail.com>
2014-02-20 00:16:27 +0200

    Added DrawEngine library class to the Project

---
+++ DrawEngine/DrawEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Game
+{
+    public static class DrawEngine
+    {
+        //Displays a single character at the given console coordinates
+        public static void PrintCharOnPosition(int posX, int posY, char c, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.ForegroundColor = color;
+            Console.Write(c);
+        }
+
+        //Displays a string at the given console coordinates
+        public static void PrintStringOnPosition(int posX, int posY, string str, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.ForegroundColor = color;
+            Console.Write(str);
+        }
+    }
+}
---
+++ Game/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game
+{
+    class Game
+    {
+        static void Main()
+        {
+            //Makes the cursor in the console invisible
+            Console.CursorVisible = false;
+        }
+    }
+}
