3e7f7aeb6822f65bc37ec8fe7ebc4d501ab81d71
shevitza <shevitza@gmail.com>
2014-02-22 09:24:20 +0200

    Game World
    World classes with test in MainWorld

---
+++ GameWorld/MainWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWorld
+{
+    class MainWorld
+    {
+        static void Main(string[] args)
+        {
+            Place wall = new Place(10, 10, 10, 2, 3 , ConsoleColor.Red);
+            Place shopFirst = new Place(2, 2, 3, 3, 2, ConsoleColor.Yellow);
+            Place wallSecond = new Place(10, 4, 2, 10, 3, ConsoleColor.DarkMagenta);
+            Place shopSecond = new Place(30, 30, 5, 5, 2, ConsoleColor.Green);
+            Place shopThird = new Place(30, 30, 5, 5, 2, ConsoleColor.Blue);
+            Place emptyPlace = new Place(18, 12, 6, 6, 0, ConsoleColor.DarkYellow);
+            List<Place> pl = new List<Place>();
+            pl.Add(wall);
+            pl.Add(shopFirst);
+            pl.Add(wallSecond);
+            pl.Add(shopSecond);
+            pl.Add(shopThird);
+            pl.Add(emptyPlace);
+       //     pl.Add(new Place(1000, 1000, 30, 30, 2, ConsoleColor.Cyan));//exception out of margins
+            World wd = new World(40, 100,pl);//width, height, list of places
+            wd.DrawPlaces();
+            //enum form { EmptySpace, Enemy, Shop, Wall };
+            //     form        0        1      2     3          
+        }
+    }
+}
---
+++ GameWorld/World.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+//xlen between 0 and 40 , yLen between 0 and 100 px
+public class World
+{
+    const int distance=10; //the distance between objects
+    private int xLen;
+    private int yLen;//Dimension of the world   
+    private List<Place> places;
+    enum form { EmptySpace, Enemy, Shop, Wall };
+
+    public World(int xLen, int yLen, List<Place> places)
+    {
+        if((this.xLen>40)||(this.xLen<0))
+        {
+            this.xLen = 40;
+        }else{
+            this.xLen = xLen;
+        }
+       if((this.yLen>100)||(this.yLen<0))
+        {
+           this.yLen=100;
+       }else{
+           this.yLen = yLen;
+       }
+        //Test if the object is  out of world margins
+       foreach (var item in places)
+       {
+           if ((item.XTopLeftPos > xLen) || (item.YTopLeftPos > yLen))
+           {
+               throw new ArgumentException("Top left corner out of World margins");
+           }
+           if((item.XLen+item.XTopLeftPos)>xLen)
+           {
+               throw new ArgumentException("Dimension X for this Place is ot of World margins");
+           }
+           if ((item.YLen + item.YTopLeftPos) > yLen)
+           {
+               throw new ArgumentException("Dimension Y for this Place is ot of World margins");
+           }
+       }
+       this.places = places;
+        
+    }
+    public int XLen
+    {
+        get
+        {
+            return this.xLen;
+        }
+    }
+    public int YLen
+    {
+        get
+        {
+            return this.yLen;
+        }
+    }
+
+    public void DrawPlaces()
+    {
+        foreach (var item in places)
+        {
+            item.Draw();
+        }
+        Console.SetCursorPosition(1, 1);
+    }
+
+}
+
