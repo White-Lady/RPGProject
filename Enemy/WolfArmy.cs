fcfd2f968c12ecf57878ce5450e420ccfef06281
anichka777 <avalcheva7@gmail.com>
2014-02-22 02:07:12 +0200

    Enemy update

--- Enemy/WolfArmy.cs
+++ Enemy/WolfArmy.cs
@@ -1,12 +1,27 @@
 ﻿namespace Enemy
 {
     using System;
+    using System.Collections.Generic;
 
     public class WolfArmy : Wolf
     {
+        private const int wolfsNumber = 5;
+
+        List<Wolf> wArmy = new List<Wolf>();
+
         public WolfArmy(int hP, int aP, int dP) 
             : base(hP, aP, dP)
         {
+        }
+
+        public void FillArmy()
+        {
+            
+        }
+
+        public void RemoveMember()
+        { 
+
         }
     }
 }
