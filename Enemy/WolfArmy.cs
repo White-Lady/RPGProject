9180cc95d05f8d634cf28d6876264da938055c12
anichka777 <avalcheva7@gmail.com>
2014-02-24 17:06:38 +0200

    Armies of enemies

--- Enemy/WolfArmy.cs
+++ Enemy/WolfArmy.cs
@@ -3,25 +3,42 @@
     using System;
     using System.Collections.Generic;
 
-    public class WolfArmy : Wolf
+    public class WolfArmy : Wolf //TODO Interface (possible IGeneric<T>) for both armies
     {
         private const int wolfsNumber = 5;
+        public bool IsFighting { get; set; }
 
         List<Wolf> wArmy = new List<Wolf>();
 
         public WolfArmy(int hP, int aP, int dP) 
             : base(hP, aP, dP)
         {
-        }
-
-        public void FillArmy()
-        {
             
         }
 
-        public void RemoveMember()
-        { 
+        private Wolf Wolf(Wolf wolf)
+        {
+            wolf = new Wolf(this.HitPoints, this.AttackPoints, this.DefensePoints);
+            return wolf;
+        }
 
+        public void FillArmy(Wolf wolf)
+        {
+            for (int i = 0; i < wolfsNumber; i++)
+            {
+                wArmy.Add(wolf);
+            }
+        }
+
+        //TODO CheckIsFigther()
+
+        public void RemoveMember(bool isDead)
+        {
+            this.IsDead = isDead;
+            if (IsDead == true)
+            {
+
+            }
         }
     }
 }
