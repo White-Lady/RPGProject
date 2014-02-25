9349281b483810dcd1e11a357e22e83e9d4fa481
anichka777 <avalcheva7@gmail.com>
2014-02-26 01:43:46 +0200

    Exception class

---
+++ Enemy/NumberOutOfRangeException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemy
+{
+    public class NumberOutOfRangeException : ApplicationException
+    {
+        private int Num { get; set; }
+
+        public NumberOutOfRangeException(string msg) 
+            : base(msg)
+	    {
+	    }
+
+        public NumberOutOfRangeException(string msg, int num) 
+            : base(msg)
+        {
+            this.Num = num;
+        }
+
+        public NumberOutOfRangeException(string msg, Exception innerEx) 
+            : base(msg, innerEx)
+	    {
+	    }
+
+        public override string Message
+        {
+            get
+            {
+                return base.Message + " " + this.Num;
+            }
+        }
+    }
+}
