ec708bd9f30ab29e78bc7ac75656f394557994c9
anichka777 <avalcheva7@gmail.com>
2014-02-25 18:39:18 +0200

    EnemyArmy abstract class

---
+++ Enemy/EnemyArmy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemy
+{
+    public abstract class EnemyArmy : Enemy
+    {
+        public abstract int EnemiesNumber { get; set; }
+        public int SingleHitPoints { get; set; }
+        public int AmountHitPoints { get; set; }
+        public int SingleAttackPoints { get; set; }
+        public int SingleDefensePoints { get; set; }
+
+        public EnemyArmy(string name, int hP, int aP, int dP) 
+            : base(name, hP, aP, dP)
+        {
+            this.HitPoints = hP * EnemiesNumber;
+            this.SingleHitPoints = hP;
+            this.AmountHitPoints = hP * EnemiesNumber;
+            this.AttackPoints = aP * EnemiesNumber;
+            this.DefensePoints = dP * EnemiesNumber;
+            this.SingleAttackPoints = aP;
+            this.SingleDefensePoints = dP;
+        }
+
+        public abstract void FillArmy();
+        public abstract void RemoveMember();
+    }
+}
